f3921b975c537de863310b4929029b66895dab3d
hmtcelik <abhmtcelik@gmail.com>
2022-01-06 01:14:10 +0300

    added map and edited sizing

--- main.py
+++ main.py
@@ -6,13 +6,36 @@ from pygame.locals import *
 
 pygame.init()
 
-pygame.display.set_caption("Way2Back")
+pygame.display.set_caption("my first game")
 
-WINDOW_SIZE = (400, 400)
-#define
+WINDOW_SIZE = (600, 400)
+
+
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
+display = pygame.Surface((300, 200))
+
 player_image = pygame.image.load('images/player.png')
+player_image.set_colorkey((255,255,255))      # remove white color background on your character
+
+grass_image = pygame.image.load("images/grass.png")
+dirt_image = pygame.image.load("images/dirt.png")
+
+TILE_SIZE = grass_image.get_width()
+
+game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','2','2','2','2','2','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['2','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','2','2'],
+            ['1','1','2','2','2','2','2','2','2','2','2','2','2','2','2','2','2','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
 
 moving_right = False
 moving_left = False
@@ -21,17 +44,29 @@ player_location = [50, 50]
 player_y_momentum = 0
 
 player_rect = pygame.Rect(player_location[0], player_location[1], player_image.get_height(), player_image.get_width())
-test_rect = pygame.Rect(100,100,100,50) # bu sayilarin anlami = (x,y,uzun kenar, kisa kenar)
+test_rect = pygame.Rect(150,150,100,50) # meaning = (x, y, long side, short side)
 
 while True:
-    screen.fill((146,244,255))
+    display.fill((146,244,255)) #background color
 
-    screen.blit(player_image, player_location)
+    #fill object on your game
+    tile_rects = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == '1':
+                display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile == '2':
+                display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
-    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
-        player_y_momentum = -player_y_momentum
-    else:
-        player_y_momentum += 0.2
+    display.blit(player_image, player_location)
+
+    player_y_momentum += 0.2
     player_location[1] += player_y_momentum
 
     if moving_right == True:
@@ -42,24 +77,30 @@ while True:
     player_rect.x = player_location[0]
     player_rect.y = player_location[1]
 
-    if player_rect.colliderect(test_rect):
-        pygame.draw.rect(screen,(255,0,0),test_rect) #parantez icinin anlami (Ekrani (renk) boyancak cisim(Rect) )
-    else:
-        pygame.draw.rect(screen,(0,0,0),test_rect)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == KEYDOWN: #yurumek icin
-            if event.key == K_RIGHT: #tusu soyluyon
+        if event.type == KEYDOWN: #for walking (keydown is when you press a button on keybord)
+            if event.key == K_RIGHT: #k_right meaning = right key (just on keybord)
                 moving_right = True
             if event.key == K_LEFT:
                 moving_left = True
-        if event.type == KEYUP: #keyup dedigi iste tusa basili tutmaktan kaldiriyon ya o
+            if event.key == K_d: # k_d meaning just button d on keybord
+                moving_right = True
+            if event.key == K_a:
+                moving_left = True
+        if event.type == KEYUP: #keyup = when you break; pressing button on keybord so key up
             if event.key == K_RIGHT:
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
+            if event.key == K_d:
+                moving_right = False
+            if event.key == K_a:
+                moving_left = False
 
+    surf = pygame.transform.scale(display, WINDOW_SIZE)
+    screen.blit(surf, (0,0))
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(60) #60 fps
